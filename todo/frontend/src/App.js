c6b3f06b35bbd35b961f63c71592d80a371d5c1c
Brian K. Vagnini <brian@vagnini.net>
2021-11-22 11:30:31 -0500

    Add React code to display API results, using dummy data.

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,3 +1,4 @@
+/* 
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +21,50 @@ function App() {
       </header>
     </div>
   );
+}
+
+export default App;
+
+*/
+
+import React, { Component } from 'react';
+
+const list = [
+  {
+    "id": 1,
+    "title": "Learn Django APIs",
+    "body": "Go through the Redbook and learn how to make APIs using Django"
+},
+{
+    "id": 2,
+    "title": "Learn how to integrate React with Django",
+    "body": "Use React as the FE and Django API as the BE - use the Redbook for this task"
+},
+{
+    "id": 3,
+    "title": "Get freelance work based on this new knowledge",
+    "body": "Take profits from this and buy private island, sipping fruity drinks on the beach"
+}
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { list };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+            </div>
+        ))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
