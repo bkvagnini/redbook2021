fce9f8ee7bcf04eb2ec8a5e20c9d73e2fed75bd3
Brian K. Vagnini <brian@vagnini.net>
2021-11-22 12:04:35 -0500

    Clean up mock data code in App.js.

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,55 +1,5 @@
-/* 
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-*/
-
 import React, { Component } from 'react';
 import axios from 'axios';
-
-//Below is the mock data, to ensure that our data displays correctly via React
-/*
-const list = [
-  {
-    "id": 1,
-    "title": "Learn Django APIs",
-    "body": "This data is from a constant called list- it does NOT pull from our API just yet."
-},
-{
-    "id": 2,
-    "title": "Learn how to integrate React with Django",
-    "body": "Use React as the FE and Django API as the BE - use the Redbook for this task"
-},
-{
-    "id": 3,
-    "title": "Get freelance work based on this new knowledge",
-    "body": "Take profits from this and buy private island, sipping fruity drinks on the beach"
-}
-]
-*/
 
 class App extends Component {
   state = {
@@ -69,12 +19,7 @@ class App extends Component {
         console.log(err);
       });
   }
-  /*
-  constructor(props) {
-    super(props);
-    this.state = { list };
-  }
-  */
+
   render() {
     return (
       <div>
@@ -87,7 +32,6 @@ class App extends Component {
       </div>
     );
   }
-
 }
 
 export default App;
